b6c01521ef5787fc94666ecb51d7c549ba7face8
garagara <ayuyeaun@gmail.com>
2022-03-27 04:34:27 +0900

    Add:画面構成

--- app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.weatherapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //viewの取得
+        val btnHKD:Button = findViewById(R.id.btnHKD)
+        val btnHKT:Button = findViewById(R.id.btnHKT)
     }
 }
