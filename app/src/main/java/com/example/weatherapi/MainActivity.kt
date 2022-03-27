8ef9ce1da1694df093234a7188b6ef5b7a5cf4dc
garagara <ayuyeaun@gmail.com>
2022-03-28 03:29:17 +0900

    Add:viewの取得

--- app/src/main/java/com/example/weatherapi/MainActivity.kt
+++ app/src/main/java/com/example/weatherapi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weatherapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         //viewの取得
         val btnHKD:Button = findViewById(R.id.btnHKD)
         val btnHKT:Button = findViewById(R.id.btnHKT)
+        val cityName:TextView = findViewById(R.id.cityName)
+        val cityWeather:TextView = findViewById(R.id.cityWeather)
+        val highTem:TextView = findViewById(R.id.highTem)
+        val lowTem:TextView = findViewById(R.id.lowTem)
+        val clear:TextView = findViewById(R.id.clear)
+
+
+
     }
 }
